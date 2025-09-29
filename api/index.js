7f69d2216abc122d7d5753f59c0a680bfdbbdfdd
moha-7 <mo297591@gmail.com>
2025-09-29 20:34:18 +0400

    fix: vercel config (nodejs20, no builds) + serverless handler

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
+import express from 'express';
 import serverless from 'serverless-http';
+import { verifyProxy } from '../verifyProxy.js';
 import app from '../server.js';
 
 export default serverless(app);
+
